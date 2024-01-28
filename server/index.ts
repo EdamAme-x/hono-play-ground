709998e1703c2974b2999bf4d52ef8ce197d6fdb
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-29 07:02:24 +0900

    add

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,31 @@
 import { Hono } from 'hono'
+import { poweredBy } from 'hono/powered-by'
+import { serve } from "@hono/node-server"
+import { serveStatic } from "@hono/node-server/serve-static"
+import { readFile } from "node:fs/promises"
+import viteConfig from "../vite.config"
+
+const isProduction = process.env["NODE_ENV"] === "production"
+let html = await readFile(isProduction ? "build/index.html" : "index.html", "utf8")
+
+if (!isProduction) {
+    html = html.replace("<head>", `<head><script type="module">import RefreshRuntime from "/@react-refresh";RefreshRuntime.injectIntoGlobalHook(window);window.$RefreshReg$ = () => {};window.$RefreshSig$ = () => (type) => type;window.__vite_plugin_react_preamble_installed__ = true;</script><script type="module" src="/@vite/client"></script>`)
+}
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.use("*", poweredBy())
+
+app.get('/hello', (c) => {
   return c.text('Hello Hono!!')
 })
 
+app.use("/assets/*", serveStatic({ root: isProduction ? "build/" : "./" })).get("/*", c => c.html(html))
+
 export default app
+
+if (isProduction) {
+    serve({ ...app, port: viteConfig.server?.port }, info => {
+        console.log(`Listening on http://localhost:${info.port}`);
+    });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import devServer from '@hono/vite-dev-server'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
     jsxImportSource: 'hono/jsx'
-  })],
+  }), devServer({
+    entry: "server",
+    exclude: [
+        /.*\.tsx?($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/@.+$/,
+        /^\/favicon\.ico$/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/
+    ],
+    injectClientScript: false,
+})],
+  server: {
+    port: 3000,
+    strictPort: true
+  },
+  build: {
+    minify: true,
+    outDir: 'dist',
+  }
 })
