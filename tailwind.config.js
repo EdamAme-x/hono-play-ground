7f9c162ae93e6ce6301f0cf335a096d1e1939743
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-29 19:15:51 +0900

    fix: remove shadcn-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ import tailwindAnimate from "tailwindcss-animate"
 
 export default {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
   prefix: "",
   theme: {
     container: {
