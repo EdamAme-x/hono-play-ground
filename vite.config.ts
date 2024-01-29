b9cfbd0877c61ea8a6b6546e7824faa99e6f2410
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-29 17:50:02 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import devServer from '@hono/vite-dev-server'
 
 export default defineConfig({
   plugins: [react({
     jsxImportSource: 'hono/jsx'
-  }), devServer({
-    entry: "server",
-    exclude: [
-        /.*\.tsx?($|\?)/,
-        /.*\.(s?css|less)($|\?)/,
-        /.*\.(svg|png)($|\?)/,
-        /^\/@.+$/,
-        /^\/favicon\.ico$/,
-        /^\/(public|assets|static)\/.+/,
-        /^\/node_modules\/.*/
-    ],
-    injectClientScript: false,
-})],
+  })],
   server: {
     port: 3000,
     strictPort: true
