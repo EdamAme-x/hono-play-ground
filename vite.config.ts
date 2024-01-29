148bbd3b6d2d655ca77f68936a655cedb0d2612d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-29 19:03:28 +0900

    feat: add shadcn-ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
   build: {
     minify: true,
     outDir: 'dist',
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
